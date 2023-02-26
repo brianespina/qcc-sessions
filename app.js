e44f842f6f84fa464532800a52e629ed92df1bca
unknown <espinabrian@gmail.com>
2023-02-26 11:04:29 +0800

    installed cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const pool = require("./db");
 const app = express();
-const port = process.env.PORT || 3000;
+const cors = require("cors");
+const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
